8f6d9011e5ea7b0f43246d37e123e9ae40959817
Ben Power <bpower.ireland@gmail.com>
2026-02-11 12:51:05 +0000

    Added ConvertToBinary

--- src/project20280/stacksqueues/BracketChecker.java
+++ src/project20280/stacksqueues/BracketChecker.java
@@ -1,5 +1,7 @@
 package project20280.stacksqueues;
 
+import java.util.Scanner;
+
 class BracketChecker {
     private final String input;
 
@@ -8,7 +10,53 @@ class BracketChecker {
     }
 
     public void check() {
-        // TODO
+        ArrayStack<Character> stack = new ArrayStack<>();
+        Scanner sc = new Scanner(input);
+        sc.useDelimiter("");
+        while (sc.hasNext()) {
+            char ch = sc.next().charAt(0);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            }
+            if (ch == ')') {
+                if (stack.isEmpty()) {
+                    System.out.println("INVALID: Missing '('");
+                    return;
+                }
+                char pop = stack.pop();
+                if (pop != '(') {
+                    System.out.println("INVALID: Missing '('");
+                    return;
+                }
+            }
+            if (ch == ']') {
+                if (stack.isEmpty()) {
+                    System.out.println("INVALID: Missing '['");
+                    return;
+                }
+                char pop = stack.pop();
+                if (pop != '[') {
+                    System.out.println("INVALID: Missing '['");
+                    return;
+                }
+            }
+            if (ch == '}') {
+                if (stack.isEmpty()) {
+                    System.out.println("INVALID: Missing '{'");
+                    return;
+                }
+                char pop = stack.pop();
+                if (pop != '{') {
+                    System.out.println("INVALID: Missing '{'");
+                    return;
+                }
+            }
+        }
+        if (!stack.isEmpty()) {
+            System.out.println("INVALID: Unclosed brackets");
+            return;
+        }
+        System.out.println("VALID");
     }
 
     public static void main(String[] args) {
@@ -19,6 +67,12 @@ class BracketChecker {
                 "a{b(c]d}e", // not correct; ] doesn't match (\n" +
                 "a[b{c}d]e}", // not correct; nothing matches final }\n" +
                 "a{b(c) ", // // not correct; Nothing matches opening {
+                "{[()]}",
+                "{[(])}",
+                "{{[[(())]]}}",
+                "][]][][[]][]][][[[",
+                "(((abc))((d)))))",
+
         };
 
         for (String input : inputs) {
